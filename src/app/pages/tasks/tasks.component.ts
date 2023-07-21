bea00818be999c0930311b74e04fcb0aa94dce1b
Kevin Davila <kfariidavila@gmail.com>
2023-07-20 22:07:38 -0500

    Finished ToDo

--- src/app/pages/tasks/tasks.component.ts
+++ src/app/pages/tasks/tasks.component.ts
@@ -1,13 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+
+interface Task {
+  name: string;
+  isCompleted: boolean;
+}
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './tasks.component.html',
-  styleUrls: ['./tasks.component.scss']
+  styleUrls: ['./tasks.component.scss'],
 })
 export class TasksComponent {
+  task = new FormControl<string>('', { nonNullable: true });
 
+  tasks = signal<Task[]>([]);
+
+  completedTasks = computed(() => {
+    const completedTasks = this.tasks().filter((task) => task.isCompleted);
+    return completedTasks;
+  });
+
+  uncompletedTasks = computed(() => {
+    const completedTasks = this.tasks().filter((task) => !task.isCompleted);
+    return completedTasks;
+  });
+
+  constructor() {
+    effect(() => {
+      if (this.uncompletedTasks().length > 3)
+        alert(
+          `Tienes ${this.uncompletedTasks().length} tareas pendientes, cuidado!`
+        );
+    });
+  }
+
+  addTask() {
+    this.tasks.update((tasks) => [
+      ...tasks,
+      { name: this.task.value, isCompleted: false },
+    ]);
+    this.task.setValue('');
+  }
+
+  toggleCompletedTask(task: Task) {
+    this.tasks.mutate((tasks) => {
+      const taskToUpdate = this.tasks().find((t) => t.name === task.name);
+      if (taskToUpdate) taskToUpdate.isCompleted = !taskToUpdate.isCompleted;
+      return tasks;
+    });
+  }
+
+  deleteTask(task: Task) {
+    this.tasks.update((tasks) => tasks.filter((t) => t.name !== task.name));
+  }
+
+  resetTask() {
+    this.tasks.set([]);
+  }
 }
